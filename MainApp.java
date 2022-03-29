7e662ae4d0d989b12ff216a0d8cc632fe0ce149b
your name <abc@gmail.com>
2022-03-29 13:22:52 +0700

    first commit

---
+++ MainApp.java
@@ -0,0 +1,27 @@
+package basicc.dev;
+/**
+ * 
+ * @author PhanTrongHieu
+ *
+ */
+
+public class MainApp {
+	public static void main(String[] args) {
+		/**
+		 * Khai báo biến
+		 */
+		String firstName = "Phan Trong";
+		String lastName = "Hieu";
+		int old = 21;
+		float markMath = 8.5f;
+		boolean passed = true ;
+		
+		System.out.println("Ho va Ten: ");
+		System.out.println(firstName +" " + lastName);
+		System.out.println("markMath: "+ markMath);
+		System.out.println("passed: "+ passed);
+		
+		final float PI = 3.14f;
+	}
+
+}
